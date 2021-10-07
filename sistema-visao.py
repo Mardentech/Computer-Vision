ee35c16032ac6ff185616ece473767776b1c46b5
Arthur Douglas <arthurdouglas.135@gmail.com>
2021-10-07 14:27:46 -0300

    Update sistema-visao.py
    Adição do thresh para contornos

--- sistema-visao.py
+++ sistema-visao.py
@@ -16,7 +16,6 @@ faceClassifier = cv2.CascadeClassifier(xml_haar_cascade)
 
     #return cv2.resize(frame, dimensions, interpolation=cv2.INTER_AREA)
 
-
 #iniciar camera
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT,100)
@@ -44,8 +43,12 @@ while not cv2.waitKey(5) & 0xFF == ord("q"):
 
     gray = cv2.cvtColor(frame_color, cv2.COLOR_BGR2GRAY)
     blur = cv2.GaussianBlur(gray, (3,3), cv2.BORDER_DEFAULT) #blur serve para diminuir o ruído de luz da imagem
-    edge = cv2.Canny(blur, 125, 175) #a imagem frame_color vai ser substituida por blur futuramente após testes
+    edge = cv2.Canny(blur, 125, 175) #os paramentros 125 e 175 de forma simplificada são a sensibilidade para detectar bordas
+    #contours, hierarchies = cv2.findContours(edge, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    ret, thresh =cv2.threshold(gray,110,255,cv2.THRESH_BINARY) #os parametros precisam ser ajustados
     
+
+
     #ajuste de parametros do sistema neural.
     face = faceClassifier.detectMultiScale(gray, scaleFactor=1.5, minSize=(150, 150), minNeighbors=1)
   
@@ -55,8 +58,7 @@ while not cv2.waitKey(5) & 0xFF == ord("q"):
         cv2.rectangle(frame_color, (x, y),  (x + w, y + h), (0, 255, 0), 2)
   
     
-    cv2.imshow("Reconhecimento Facial - ITF AUTOMACAO AMBEV", frame_color)
-
-    cv2.imshow("gray",gray) 
+    #cv2.imshow("Reconhecimento Facial - ITF AUTOMACAO AMBEV", frame_color)
+    #cv2.imshow("gray",gray) 
     cv2.imshow("edge",edge)   
-
+    cv2.imshow("thresh",thresh)
